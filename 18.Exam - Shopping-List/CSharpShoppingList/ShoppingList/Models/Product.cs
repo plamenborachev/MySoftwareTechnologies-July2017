f111f4f436504394195361c4368c4c7d5e4179b3
plamenborachev <plamen.borachev@gmail.com>
2017-09-10 23:13:27 +0300

    Add files via upload

---
+++ 18.Exam - Shopping-List/CSharpShoppingList/ShoppingList/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace ShoppingList.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [AllowHtml]
+        public int Priority { get; set; }
+
+        [Required]
+        [AllowHtml]
+        public string Name { get; set; }
+
+        [Required]
+        [AllowHtml]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+    }
+}
